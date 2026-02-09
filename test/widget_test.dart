da71a4bfee16b19849cd70221d3c4c61f03934af
stark1525-ytbot <alphamovies@proton.me>
2026-02-09 15:05:51 +0530

    Add files via upload

---
+++ test/widget_test.dart
@@ -0,0 +1,22 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:fair_adda/main.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() {
+  testWidgets('App starts successfully', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const ProviderScope(child: FairAddaApp()));
+
+    // Verify that the app starts without errors.
+    expect(find.byType(Scaffold), findsWidgets);
+  });
+}
